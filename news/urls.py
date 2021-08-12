3b8f4224be0d40e135ac660972f6c4faec4de5cb
Ankush Chavan <ankush.chavan@velotio.com>
2021-08-12 16:19:20 +0530

    Templates removed and news app deleted
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # Setting dynamic url
-    # Accept name of the reporter through URL and store it in reporter_name variable. Type of the variable is string.
-    path('<str:reporter_name>/', views.reporter),
-]
