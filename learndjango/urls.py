b7e7fbc7c78e0e287d648178dc94587637bb1722
Ankush Chavan <ankush.chavan@velotio.com>
2021-08-13 14:58:21 +0530

    jwtauth urls removed
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- learndjango/urls.py
+++ learndjango/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('jwtauth.urls')),
     # Complete authentication using custom JWT token
     path('api/', include('users.urls')),
 ]
