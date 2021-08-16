c245890322f922fcc9555c3d20eb9fb4bf1f7393
Ankush Chavan <ankush.chavan@velotio.com>
2021-08-16 15:39:40 +0530

    Authorization check added for TodoListView and filter the todo data based on currently authorized user
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- server/apps/todo/views.py
+++ server/apps/todo/views.py
@@ -1,13 +1,31 @@
+import jwt
+
+from django.conf import settings
+from django.db.models import Q
+from rest_framework.exceptions import NotAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Todo
 from .serializer import TodoSerializer
+from ..users.models import User
 
 
 class TodoListView(APIView):
     @staticmethod
     def get(request):
-        todos = Todo.objects.all()
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise NotAuthenticated('Unauthenticated!')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise NotAuthenticated('Authentication failed! Signature Expired!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        todos = Todo.objects.filter(Q(owner=user.name) | Q(editors__contains=[user.name])).all()
+
         serializer = TodoSerializer(todos, many=True)
+
         return Response(serializer.data)
