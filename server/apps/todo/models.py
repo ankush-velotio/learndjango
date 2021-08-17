5ff83d392d05cf35d9468d35a470edc4cfd33f5e
Ankush Chavan <ankush.chavan@velotio.com>
2021-08-17 11:52:11 +0530

    Abstact models for time stamped info and creator info are implemented
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- server/apps/todo/models.py
+++ server/apps/todo/models.py
@@ -2,19 +2,17 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.utils.timezone import now
 from model_utils import Choices
-from timestampedmodel import TimestampedModel
+from ..common.mode_utils import TimestampedModel, CreatorInfoModel
 
 from ..users.models import User
 
 
-class Todo(TimestampedModel):
+class Todo(TimestampedModel, CreatorInfoModel):
     COMPLETED = "completed"
     INPROGRESS = "inprogress"
     STATUS = Choices((COMPLETED, COMPLETED), (INPROGRESS, INPROGRESS))
     status = models.CharField(choices=STATUS, default=INPROGRESS, max_length=20)
     owner = models.CharField(max_length=100)
-    created_by = models.CharField(max_length=100)
-    updated_by = models.CharField(max_length=100, blank=True, null=True)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     date = models.DateTimeField(default=now)
