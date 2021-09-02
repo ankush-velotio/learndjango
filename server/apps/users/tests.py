d90588355694cb8836d4ec20d266e7e089a67afe
Ankush Chavan <ankush.chavan@velotio.com>
2021-09-02 11:01:19 +0530

    refactor(get jwt token): Getting JWT token from current session and forcing logout test to run after login test
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

--- server/apps/users/tests.py
+++ server/apps/users/tests.py
@@ -9,7 +9,7 @@ from users.views import RegisterView, LoginView, UserView, LogoutView
 
 
 class UserTest(TestCase):
-    jwt_token: str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFua3VzaEBnbWFpbC5jb20iLCJleHAiOjE2MzA1MTA3NzMsImVtYWlsIjoiYW5rdXNoQGdtYWlsLmNvbSIsIm9yaWdfaWF0IjoxNjMwNDk4NzczfQ.o-LF1qwuV2TEoEKzkgX4WTv-I8CybgT9xM9StKaGPnA"
+    jwt_token: str = ""
 
     @classmethod
     def setUpClass(cls):
@@ -35,17 +35,6 @@ class UserTest(TestCase):
         self.assertEqual(user_data['email'], response.data['email'])
         self.assertEqual(user_data['name'], response.data['name'])
 
-    def test_login_view(self):
-        user_data = {
-          "email": "demo@gmail.com",
-          "password": "pass"
-        }
-        request = HttpRequest()
-        request.data = user_data
-        response = LoginView.post(self, request=request)
-        self.jwt_token = response.cookies.get('jwt')
-        self.assertEqual(AUTHENTICATION_SUCCESSFUL, response.data["message"])
-
     def test_user_view(self):
         required_result = {
           "id": 1,
@@ -57,8 +46,23 @@ class UserTest(TestCase):
         response = UserView.get(request=request)
         self.assertJSONEqual(json.dumps(required_result), response.data)
 
-    def test_logout_view(self):
+    def login_view(self):
+        user_data = {
+          "email": "demo@gmail.com",
+          "password": "pass"
+        }
+        request = HttpRequest()
+        request.data = user_data
+        response = LoginView.post(self, request=request)
+        self.jwt_token = response.cookies.get('jwt').__str__().split(';')[0].split('Set-Cookie: jwt=')[1]
+        self.assertEqual(AUTHENTICATION_SUCCESSFUL, response.data["message"])
+
+    def logout_view(self):
         request = HttpRequest()
         request.COOKIES.setdefault('jwt', self.jwt_token)
         response = LogoutView.post(request=request)
         self.assertEqual(LOGOUT_SUCCESSFUL, response.data['message'])
+
+    def test_login_then_logout(self):
+        self.login_view()
+        self.logout_view()
